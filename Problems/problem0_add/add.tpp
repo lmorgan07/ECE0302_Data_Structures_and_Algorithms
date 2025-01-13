502b4c2cfbbd0ee0a123ef665222be9c89bf82a7
Luke Morgan <lmm329@pitt.edu>
2025-01-13 17:18:14 +0000

    Pending changes exported from your codespace

--- Problems/problem0_add/add.tpp
+++ Problems/problem0_add/add.tpp
@@ -9,5 +9,6 @@ Add<T>::~Add() {}
 template <typename T>
 T Add<T>::call(T arg1, T arg2) const
 {
-  // TODO
+
+  return arg1 + arg2;
 }
--- Problems/problem0_add/test_add.cpp
+++ Problems/problem0_add/test_add.cpp
@@ -6,8 +6,49 @@
 // force template expansion
 template class Add<int>;
 
-TEST_CASE("Test add", "[Add]"){
+TEST_CASE("Test add", "[Add]")
+{
 
   Add<int> a;
-  REQUIRE(a.call(1,2)==3);
+  REQUIRE(a.call(1, 2) == 3);
+}
+
+// Long Add
+TEST_CASE("Test long", "[Add]")
+{
+  Add<int> l;
+  REQUIRE(l.call(1111111, 2222222) == 3333333);
+}
+
+// double add
+TEST_CASE("Test double", "[Add]")
+{
+  double THRESHOLD = 0.000000001;
+  Add<double> d;
+  REQUIRE((d.call(3, -sqrt(2)) - 1.58578644) <= THRESHOLD);
+  REQUIRE((d.call(4, sqrt(2)) - 5.41421356237) <= THRESHOLD);
+}
+
+// char add
+TEST_CASE("Test Char", "[ADD]")
+{
+  Add<char> c;
+  // ASCII # of '2' is 50 and '3' is 51, so adding them is equal to 101
+  REQUIRE(c.call('2', '3') == 101);
+}
+
+// string add
+TEST_CASE("Test String", "[ADD]")
+{
+  Add<std::string> s;
+  // strings concatenate with addition
+  REQUIRE(s.call("2", "3") == "23");
+}
+
+// bool add
+TEST_CASE("Test Bool", "[Add]")
+{
+  Add<bool> b;
+  // adding two truths together returns a true
+  REQUIRE(b.call(1, 1) == 1);
 }
